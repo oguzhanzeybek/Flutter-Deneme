5e6c5b4bfa16007c67cdb7ef759410c857f2fb7f
oguzhanzeybek <darkstroge@gmail.com>
2022-12-08 15:58:12 +0300

    labeltext

--- kosmos/deneme9/lib/login_page.dart
+++ kosmos/deneme9/lib/login_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -15,10 +17,20 @@ class _LoginPageState extends State<LoginPage> {
       body: Form(
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [Padding(
-          padding: const EdgeInsets.only(left: 20,right: 20.0),
-          child: TextFormField(),
-        )],
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.purple),
+                ),
+                labelText: "KULLANICI ADI",
+                labelStyle: TextStyle(color: Colors.purple),
+              ),
+            ),
+          )
+        ],
       )),
     );
   }
