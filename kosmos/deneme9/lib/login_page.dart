43915f181a0bc572f67bbcbd0c1fd9268ce805d0
oguzhanzeybek <darkstroge@gmail.com>
2022-12-08 16:49:33 +0300

    Eelevatedbuton

--- kosmos/deneme9/lib/login_page.dart
+++ kosmos/deneme9/lib/login_page.dart
@@ -10,38 +10,85 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  late String username;
+  late String password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Form(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20.0),
-            child: TextFormField(
+          child: Padding(
+        padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
               //autovalidateMode: true,
               decoration: InputDecoration(
                 focusedBorder: OutlineInputBorder(
                   borderSide: BorderSide(color: Colors.purple),
                 ),
-                labelText: "KULLANICI ADI",
+                labelText: "Kullanıcı Adı",
                 labelStyle: TextStyle(color: Colors.purple),
                 border: OutlineInputBorder(),
               ),
               validator: ((value) {
                 if (value == "") {
                   return "kullanıcı adını giriniz.";
-                }
-                else{
+                } else {
                   return null;
                 }
               }),
+              onSaved: (value) {
+                username = value!;
+              },
             ),
-          )
-        ],
+            SizedBox(
+              height: 10.0,
+            ),
+            TextFormField(
+              //autovalidateMode: true,
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.purple),
+                ),
+                labelText: "Şifre",
+                labelStyle: TextStyle(color: Colors.purple),
+                border: OutlineInputBorder(),
+              ),
+              validator: ((value) {
+                if (value == "") {
+                  return "Şifrenizi giriniz.";
+                } else {
+                  return null;
+                }
+              }),
+              onSaved: (value) {
+                username = value!;
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                MaterialButton(child: Text("Üye ol"),onPressed: () {},
+                ),
+                MaterialButton(child: Text("Şifremi unuttum "),onPressed: () {},
+                ),
+              ],
+              
+            ),
+            _loginButton()
+          ],
+        ),
       )),
     );
   }
+
+
+  Widget _loginButton() => ElevatedButton(onPressed: (){}, child: Text("Giriş Yap"),);
+    
+
+
+
 }
