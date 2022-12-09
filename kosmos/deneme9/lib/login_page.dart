0d5cd5570ad30b229523a704afb2975f301eeb9f
oguzhanzeybek <darkstroge@gmail.com>
2022-12-09 05:01:13 +0300

    uı kısımı tasarım denemesi

--- kosmos/deneme9/lib/login_page.dart
+++ kosmos/deneme9/lib/login_page.dart
@@ -1,5 +1,3 @@
-
-
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
@@ -12,115 +10,139 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-   late String password = "kullanıcı adı yanlıs";
-   late String  username = "Kullanıcı adı ";
-   
+  late String password = "kullanıcı adı yanlıs";
+  late String username = "Kullanıcı adı ";
+
   final _formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Form(
+      body:   Form( 
           key: _formkey,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+
+          
+          
+          
+            
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-
-
-
-                TextFormField(
-                  //autovalidateMode: true,
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.purple),
+                Column( crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Image( 
+                      fit: BoxFit.cover,
+                      image: AssetImage("assets/images/ustfoto.png",),),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    //autovalidateMode: true,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.purple),
+                      ),
+                      labelText: "Kullanıcı Adı",
+                      labelStyle: TextStyle(color: Colors.purple),
+                      border: OutlineInputBorder(),
                     ),
-                    labelText: "Kullanıcı Adı",
-                    labelStyle: TextStyle(color: Colors.purple),
-                    border: OutlineInputBorder(),
+                    validator: ((value) {
+                      if (value == "") {
+                        return "kullanıcı adını giriniz.";
+                      } else {
+                        return null;
+                      }
+                    }),
+                    onSaved: (value) {
+                      username = value!;
+                    },
                   ),
-                  validator: ((value) {
-                    if (value == "") {
-                      return "kullanıcı adını giriniz.";
-                    } else {
-                      return null;
-                    }
-                  }),
-                  onSaved: (value) {
-                    username = value!;
-                  },
                 ),
                 SizedBox(
                   height: 10.0,
                 ),
-
-
-                TextFormField(
-                  //autovalidateMode: true,
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.purple),
+                
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    //autovalidateMode: true,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.purple),
+                      ),
+                      labelText: "Şifre",
+                      labelStyle: TextStyle(color: Colors.purple),
+                      border: OutlineInputBorder(),
                     ),
-                    labelText: "Şifre",
-                    labelStyle: TextStyle(color: Colors.purple),
-                    border: OutlineInputBorder(),
+                    validator: ((value) {
+                      if (value == "") {
+                        return "Şifrenizi giriniz.";
+                      } else {
+                        return null;
+                      }
+                    }),
+                    onSaved: (value) {
+                      password = value!;
+                    },
                   ),
-                  validator: ((value) {
-                    if (value == "") {
-                      return "Şifrenizi giriniz.";
-                    } else {
-                      return null;
-                    }
-                  }),
-                  onSaved: (value) {
-                    password = value!;
-                  },
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      MaterialButton(
+                        child: Text("Giriş Yap"),
+                        onPressed: () {},
+                      ),
+                      MaterialButton(
+                        child: Text("Şifremi unuttum "),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+                _loginButton(),
+                Column( crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
-                    MaterialButton(
-                      child: Text("Giriş Yap"),
-                      onPressed: () {},
-                    ),
-                    MaterialButton(
-                      child: Text("Şifremi unuttum "),
-                      onPressed: () {},
-                    ),
+                    Image( 
+                      fit: BoxFit.cover,
+                      image: AssetImage("assets/images/altfoto.png",),),
                   ],
                 ),
-                _loginButton()
               ],
             ),
-          )),
+          ),
+          
     );
   }
 
-  Widget _loginButton ()  => ElevatedButton(
-         onPressed: () async {
+  Widget _loginButton() => ElevatedButton(
+        onPressed: () async {
           if (_formkey.currentState!.validate()) {
             _formkey.currentState!.save();
-            if(username == "a" && password == "b"){
+            if (username == "a" && password == "b") {
               debugPrint("giriş başarılı");
-            } else{
-              showDialog(barrierDismissible: false,context: context, builder: (BuildContext  context) { 
-                return AlertDialog(
-                  title: Text("Hata!"),
-                  content: Text("giriş bilgileriniz hatalı"),
-                  actions: [
-                    MaterialButton(child: Text("geri dön"),onPressed:(() => Navigator.pop(context)))
-                  ],
-                );
-                
-              });
+            } else {
+              showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text("Hata!"),
+                      content: Text("giriş bilgileriniz hatalı"),
+                      actions: [
+                        MaterialButton(
+                            child: Text("geri dön"),
+                            onPressed: (() => Navigator.pop(context)))
+                      ],
+                    );
+                  });
             }
           }
         },
         child: Text("Giriş Yap"),
-        
       );
 }
-
-
