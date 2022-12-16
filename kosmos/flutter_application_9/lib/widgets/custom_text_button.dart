c151fdc3ff6d46608c4034a2fdc768aafc6b9ef4
oguzhanzeybek <darkstroge@gmail.com>
2022-12-16 23:30:00 +0300

    uyarı mesajları cözümü

--- kosmos/flutter_application_9/lib/widgets/custom_text_button.dart
+++ kosmos/flutter_application_9/lib/widgets/custom_text_button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_application_9/sign_up.dart';
+
+
+
 
 class CustomTextButton extends StatelessWidget {
   final VoidCallback onPressed;
